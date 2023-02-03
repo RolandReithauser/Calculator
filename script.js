d0b432533bc64df664f4a78007c40e6062253c96
RolandReithauser <reithauser@gmail.com>
2023-02-03 09:57:10 +0100

    - Less functions, but these are work (only BS not)

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-function send(){
-    let buttonValue = document.getElementById('btn').value;
-    
+function display(val){
+    document.getElementById('result').value += val
+    return val
 }
-/*
-btn CE
-btn C
-btn BS
-btn division
-btn seven
-btn eight
-btn nine
-btn multiplication
-btn four
-btn five
-btn six
-btn subtraction
-btn one
-btn two
-btn three
-btn addition
-btn sign
-btn zero
-btn dot
-btn equal
-*/
+
+function solve(){
+    let x = document.getElementById('result').value
+    let y = eval(x);
+    document.getElementById('result').value = y
+    return y
+}
+
+function clearScreen(){
+    document.getElementById('result').value = ''
+}
+
+function backSpace(){
+    document.getElementById('result').value = pop(result)
+}
